d2db0ea32cff5f5972f0200761b632f49aceb354
Artemiy <alistan123@bk.ru>
2024-04-13 18:32:35 +0400

    Migration for refactor tables

---
+++ BannerService/Migrations/20240413143147_RefactorTables.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BannerService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Banners_Features_FeaturesId",
+                table: "Banners");
+
+            migrationBuilder.RenameColumn(
+                name: "FeaturesId",
+                table: "Banners",
+                newName: "FeatureId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Banners_FeaturesId",
+                table: "Banners",
+                newName: "IX_Banners_FeatureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Banners_Features_FeatureId",
+                table: "Banners",
+                column: "FeatureId",
+                principalTable: "Features",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Banners_Features_FeatureId",
+                table: "Banners");
+
+            migrationBuilder.RenameColumn(
+                name: "FeatureId",
+                table: "Banners",
+                newName: "FeaturesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Banners_FeatureId",
+                table: "Banners",
+                newName: "IX_Banners_FeaturesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Banners_Features_FeaturesId",
+                table: "Banners",
+                column: "FeaturesId",
+                principalTable: "Features",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
